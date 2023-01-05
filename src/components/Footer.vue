ae7df69d23663eba2661b3d2c166f597706fde91
Aladaner <solovej555@gmail.com>
2023-01-05 17:18:13 +0200

    footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,24 +1,36 @@
 <template>
 	<footer class="footer">
 		<div class="footer__container container">
+			
 			<div class="footer__logo">
 				<div class="footer__logo-title">HouseVOP</div>
 				<div class="footer__logo-subtitle">ЖИЛОЙ КОМПЛЕКС</div>
 			</div>
 
-			<div class="footer__menu">
+			<div class="footer__menu menu-1">
 				<ul class="footer__menu-list">
-					<li class="footer__menu-item" v-for="(item, idx) in FooterList" :key="idx">
+					<li class="footer__menu-item" v-for="(item, idx) in FooterMenuList" :key="idx">
 						<router-link to="/">{{ item.text }}</router-link>
 					</li>
 				</ul>
 			</div>
-			<div class="footer__menu">
-				<ul class="footer__menu-list"></ul>
+			
+			<div class="footer__menu menu-2">
+				<ul class="footer__menu-list">
+					<li class="footer__menu-item" v-for="(item, idx) in FooterMenuList" :key="idx">
+						<router-link to="/">{{ item.text }}</router-link>
+					</li>
+				</ul>
 			</div>
-			<div class="footer__menu">
-				<ul class="footer__menu-list"></ul>
+
+			<div class="footer__contacts">
+				<ul class="footer__list">
+					<li class="footer__item" v-for="(item, idx) in FooterContactList" :key="idx">
+						<span>{{ item.name }}</span> <a href="">{{ item.text }}</a>
+					</li>
+				</ul>
 			</div>
+			
 		</div>
 	</footer>
 </template>
@@ -27,7 +39,7 @@
 	export default {
 		data() {
 			return {
-				FooterList: [
+				FooterMenuList: [
 					{						
 						text: "О комплексе"
 					},
@@ -44,6 +56,24 @@
 					{
 						text: "Контакты"
 					},
+				],
+				FooterContactList: [
+					{
+						name: "Адрес:",
+						text: "Київ, вул Кондукторська, 39"
+					},
+					{
+						name: "Телефон:",
+						text: "+380 000 00 00"
+					},
+					{
+						name: "Отдел продаж",
+						text: "10:00 - 20:00"
+					},
+					{
+						name: "E-mail:",
+						text: "test@gmail.com"
+					}
 				]
 			}
 		}
