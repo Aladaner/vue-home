4cea445aff8e8dfb4c3409c9e97f1fd42da349b9
Aladaner <solovej555@gmail.com>
2023-01-05 22:45:06 +0200

    vue.config update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-home'
+    : '/'
+	,
 	transpileDependencies: true,
 	css: {
 		loaderOptions: {
